c40cb4512396533f33134330d5c7106974ff9ebe
MountCao <1246984093@qq.com>
2019-03-23 14:49:29 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div 
+                    class="icon" 
+                    v-for="item of page" 
+                    :key="item.id"
+                >
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl"/>
+                        <p class="icon-desc">{{item.desc}}</p>
+                    </div>
+                </div>  
+            </swiper-slide>   
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeIcons',
+    data() {
+        return {
+            iconList: [{
+                id: '0001',
+                imgUrl: '//pic5.40017.cn/01/000/5f/30/rBANC1qozNqAb3vRAAAD_Qg-dt4234.png',
+                desc: '火车票'       
+            },{
+                id: '0002',
+                imgUrl: '//pic5.40017.cn/02/001/5f/ab/rBLkCFqozNqAF4UUAAAFH-2jq70957.png',
+                desc: '热门景点'       
+            },{
+                id: '0003',
+                imgUrl: '//pic5.40017.cn/02/001/5f/ab/rBLkCFqozNqAJqPYAAAFlYq1v7A413.png',
+                desc: '飞行机票'       
+            },{
+                id: '0004',
+                imgUrl: '//pic5.40017.cn/01/000/5f/a5/rBLkBVqozNqAGbVbAAADpuwJ0xY082.png',
+                desc: '酒店住宿'       
+            },{
+                id: '0005',
+                imgUrl: '//pic5.40017.cn/01/000/5f/30/rBANC1qozNqAb3vRAAAD_Qg-dt4234.png',
+                desc: '火车票'       
+            },{
+                id: '0006',
+                imgUrl: '//pic5.40017.cn/02/001/5f/ab/rBLkCFqozNqAF4UUAAAFH-2jq70957.png',
+                desc: '热门景点'       
+            },{
+                id: '0007',
+                imgUrl: '//pic5.40017.cn/02/001/5f/ab/rBLkCFqozNqAJqPYAAAFlYq1v7A413.png',
+                desc: '飞行机票'       
+            },{
+                id: '0008',
+                imgUrl: '//pic5.40017.cn/01/000/5f/a5/rBLkBVqozNqAGbVbAAADpuwJ0xY082.png',
+                desc: '酒店住宿'       
+            },{
+                id: '0009',
+                imgUrl: '//pic5.40017.cn/01/000/5f/a5/rBLkBVqozNqAGbVbAAADpuwJ0xY082.png',
+                desc: '酒店住宿'       
+            }
+            ]
+        }
+    },
+    computed: {
+        pages(){
+            const pages = []
+            this.iconList.forEach((item,index)=> {
+                const page = Math.floor(index / 8)
+                if(!pages[page]){
+                    pages[page]=[]
+                }
+                pages[page].push(item)
+            })
+            return pages
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height: 0
+        padding-bottom: 50%
+    .icon
+        position: relative
+        overflow: hidden
+        float: left
+        width: 25%
+        height: 0
+        padding-bottom: 25%
+        .icon-img
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            buttom: 0.44rem
+            box-sizing: border-box
+            padding: .1rem
+            .icon-img-content
+                display:block
+                margin: 0 auto
+                height: 1.2rem
+        .icon-desc
+            position: absolute
+            left: 0
+            right: 0
+            top: 1.35rem
+            height: 0.4rem
+            line-height: 0.48rem
+            color: darkTextColor
+            text-align: center
+            ellipsis()    
+</style>
