af94ab1e7e2deb1f1b4bd72ea14a915a660943bd
MountCao <1246984093@qq.com>
2019-03-24 19:58:38 +0800

    city-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       // 当用户访问的是根路径的时候，我们给用户展示的组件是HelloWorld这个组件
       path: '/',
       name: 'Home',
       component: Home
-    },
-  ]
+    },{
+      path: '/city',
+      name: 'City',
+      component: City
+    }]
 })
