4d537e84be13682d40b30db53f123c2441ec5830
MountCao <1246984093@qq.com>
2019-03-24 11:35:31 +0800

    add recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -84,33 +84,36 @@ export default {
     .icons >>> .swiper-container
         height: 0
         padding-bottom: 50%
-    .icon
-        position: relative
-        overflow: hidden
-        float: left
-        width: 25%
-        height: 0
-        padding-bottom: 25%
-        .icon-img
-            position: absolute
-            top: 0
-            left: 0
-            right: 0
-            buttom: 0.44rem
-            box-sizing: border-box
-            padding: .1rem
-            .icon-img-content
-                display:block
-                margin: 0 auto
-                height: 1.2rem
-        .icon-desc
-            position: absolute
-            left: 0
-            right: 0
-            top: 1.35rem
-            height: 0.4rem
-            line-height: 0.48rem
-            color: darkTextColor
-            text-align: center
-            ellipsis()    
+    .icons
+        margin-top .1rem
+        touch-action: none;
+        .icon
+            position: relative
+            overflow: hidden
+            float: left
+            width: 25%
+            height: 0
+            padding-bottom: 25%
+            .icon-img
+                position: absolute
+                top: 0
+                left: 0
+                right: 0
+                buttom: 0.44rem
+                box-sizing: border-box
+                padding: .1rem
+                .icon-img-content
+                    display:block
+                    margin: 0 auto
+                    height: 1.2rem
+            .icon-desc
+                position: absolute
+                left: 0
+                right: 0
+                top: 1.35rem
+                height: 0.4rem
+                line-height: 0.48rem
+                color: darkTextColor
+                text-align: center
+                ellipsis()    
 </style>
