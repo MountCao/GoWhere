ebb1a0a05db83c416a528576447d4e337ae38cf1
MountCao <1246984093@qq.com>
2019-03-29 21:44:33 +0800

    '数据放到服务器上'

--- config/index.js
+++ config/index.js
@@ -12,15 +12,15 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://0.0.0.0:8080',
+        target: 'http://code200.cn',
         pathRewrite: {
-          '^/api': '/static/mock'
+          '^/api': 'api'
         }
       }
     },
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/travel',
 
     /**
      * Source Maps
